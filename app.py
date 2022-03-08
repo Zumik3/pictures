a0c097a21b32db1ab8433588d95c4edd1301caa1
zum <p.dinkevich@mail.ru>
2022-03-08 15:39:29 +0300

    refactoring base64

--- app.py
+++ app.py
@@ -31,12 +31,12 @@ def unauthorized():
     return resp(401, {'error': 'Unauthorized access'})
 
 
-def string_to_base64(s):
-    return base64.b64encode(s.encode('utf-8'))
-
-
-def base64_to_string(b):
-    return base64.b64decode(b).decode('utf-8')
+# def string_to_base64(s):
+#     return base64.b64encode(s.encode('utf-8'))
+#
+#
+# def base64_to_string(b):
+#     return base64.b64decode(b).decode('utf-8')
 
 
 def to_json(data):
